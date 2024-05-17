6c03563a375a9b3b1b7f31b4bc12fbf6ccef213b
Lucas Dietrich <ld.adecy@gmail.com>
2024-05-17 20:47:58 +0200

    Fix devices array

--- ui/src/components/DevicesArray.tsx
+++ ui/src/components/DevicesArray.tsx
@@ -1,58 +1,134 @@
-// @ts-check
-
 import { DevicesList } from "@caniot-controller/caniot-api-grpc-web/api/ng_devices_pb";
 import { Timestamp } from "google-protobuf/google/protobuf/timestamp_pb";
-import { Table } from "antd";
+import { Table, TableProps, Button, Space, Tag } from "antd";
 
 interface IProps {
   devicesList: DevicesList | undefined;
 }
+
+interface DeviceRow {
+  key: string;
+  did: number;
+  tags: {
+    sid: number;
+    cls: number;
+  };
+  controller?: string;
+  last_seen?: {
+    timestamp: Timestamp;
+    secondsFromNow: number;
+  };
+  temp_in?: number;
+}
+
+const classColor: { [key: number]: string } = {
+  0: "red",
+  1: "green",
+  2: "purple",
+  3: "orange",
+  4: "cyan",
+  5: "magenta",
+  6: "geekblue",
+  7: "orange",
+};
 
 function DevicesTable({ devicesList }: IProps) {
   if (devicesList === undefined) {
     return undefined;
   }
 
-  const columns = [
+  const columns: TableProps<DeviceRow>["columns"] = [
     {
-      title: "Device ID",
+      title: "Device Id",
       dataIndex: "did",
       key: "did",
+      render: (did) => <span>{did}</span>,
+      sorter: (a, b) => a.did - b.did,
+    },
+    {
+      title: "Tags",
+      dataIndex: "tags",
+      key: "tags",
+      render: ({ cls, sid }, record) => (
+        <span>
+          <Tag color={classColor[cls]}>Class {cls}</Tag>
+          <Tag color="blue">id {sid}</Tag>
+        </span>
+      ),
+    },
+    {
+      title: "Controller",
+      dataIndex: "controller",
+      key: "controller",
+      render: (controller) => <span>{controller || "N/A"}</span>,
     },
     {
       title: "Last seen",
       dataIndex: "last_seen",
       key: "last_seen",
+      render: (last_seen) => (
+        <span>
+          {last_seen ? last_seen.timestamp.toDate().toLocaleString() : "never"} (
+          {last_seen ? last_seen.secondsFromNow : ""} ago)
+        </span>
+      ),
+      sorter: (a, b) => {
+        if (a.last_seen && b.last_seen) {
+          return a.last_seen.secondsFromNow - b.last_seen.secondsFromNow;
+        } else if (a.last_seen) {
+          return -1;
+        } else if (b.last_seen) {
+          return 1;
+        } else {
+          return 0;
+        }
+      },
     },
     {
-      title: "Contrôleur",
-      dataIndex: "controller",
-      key: "controller",
-    },
-    {
-      title: "Temperature",
+      title: "Temp In",
       dataIndex: "temp_in",
       key: "temp_in",
+      render: (temp_in) => <span>{temp_in?.toFixed(2) || "N/A"} °C</span>,
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="small">
+          <Button type="primary" size="small">
+            Reset
+          </Button>
+          <Button type="primary" size="small">
+            Reset settings
+          </Button>
+          <Button type="primary" size="small">
+            Reset settings
+          </Button>
+        </Space>
+      ),
     },
   ];
 
-  const dataSource = devicesList.getDevicesList().map((device, index) => {
-    let lastseen: Timestamp | undefined = device.getLastseen();
-    let lastseen_fmt = lastseen?.toDate().toLocaleString();
-
-    let temp = device.getClass1()?.getIntTemp().toFixed(2) || "N/A";
-
+  const data = devicesList.getDevicesList().map((device, index) => {
     return {
-      key: index,
-      did: device.getDid()?.getCls() + " / " + device.getDid()?.getSid(),
-      last_seen:
-        (lastseen_fmt || "").toString() + " ( il y a " + device.getLastseenfromnow() + "s )",
-      temp_in: temp + " °C",
-      controller: device.getControllerAttached() ? device.getControllerName() : "N/A",
+      key: index.toString(),
+      did: device.getDid()?.getDid() || 0,
+      tags: {
+        cls: device.getDid()?.getCls() || 0,
+        sid: device.getDid()?.getSid() || 0,
+      },
+      controller: device.getControllerAttached() ? device.getControllerName() : undefined,
+      last_seen: device.getLastseen()
+        ? {
+            timestamp: device.getLastseen() || new Timestamp(),
+            secondsFromNow: device.getLastseenfromnow(),
+          }
+        : undefined,
+      temp_in: device.getClass1()?.getIntTemp(),
     };
   });
 
-  return <Table dataSource={dataSource} columns={columns} />;
+  return <Table columns={columns} dataSource={data} />;
 }
 
 export default DevicesTable;
