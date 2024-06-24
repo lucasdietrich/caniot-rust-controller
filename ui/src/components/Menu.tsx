cefcf7fa7d17d2da361975983413ccf63012ca90
Lucas Dietrich <ld.adecy@gmail.com>
2024-06-24 18:37:23 +0200

    Rework main menu

--- ui/src/components/Menu.tsx
+++ ui/src/components/Menu.tsx
@@ -6,66 +6,66 @@ import {
   PercentageOutlined,
   ControlOutlined,
   SettingFilled,
+  PlusOutlined,
 } from "@ant-design/icons";
 import { Settings } from "@caniot-controller/caniot-api-grpc-web/api/ng_internal_pb";
+import { LuSiren } from "react-icons/lu";
+import { MdOutlineGarage } from "react-icons/md";
+import { LiaTemperatureLowSolid } from "react-icons/lia";
 
 interface IMenuProps {
   settings?: Settings;
 }
 
 function AppMenu({ settings }: IMenuProps) {
-  const homeChildren = [
-    { key: "1", label: <Link to="/">Aperçu</Link> },
-    { key: "2", label: <Link to="/devices">Appareils</Link> },
-  ];
-
-  if (settings?.getDebugMode()) {
-    homeChildren.push({ key: "3", label: <Link to="/debug">Debug</Link> });
-  }
-
   const menuItems: MenuProps["items"] = [
     {
       key: "home",
       icon: <HomeOutlined />,
-      label: "Home",
-      children: homeChildren,
+      label: <Link to="/">Aperçu</Link>,
     },
     {
-      key: "devices",
-      icon: <ControlOutlined />,
-      label: "Contrôleurs",
-      children: [
-        {
-          key: "10",
-          label: <Link to="/devices/heaters">Chauffages</Link>,
-        },
-        { key: "11", label: <Link to="/devices/garage">Portes de garage</Link> },
-        { key: "12", label: <Link to="/devices/alarms">Alarmes</Link> },
-      ],
+      key: "garage",
+      label: <Link to="/devices/garage">Portes de garage</Link>,
+      icon: <MdOutlineGarage />,
+    },
+    { key: "alarms", label: <Link to="/devices/alarms">Alarmes</Link>, icon: <LuSiren /> },
+    {
+      key: "heaters",
+      label: <Link to="/devices/heaters">Chauffages</Link>,
+      icon: <LiaTemperatureLowSolid />,
     },
     {
-      key: "measures",
-      icon: <PercentageOutlined />,
-      label: "Mesures",
-      children: [{ key: "6", label: <Link to="/sensors">Capteurs</Link> }],
-    },
-    {
-      key: "misc",
+      key: "settings",
+      label: <Link to="/settings">Configuration</Link>,
       icon: <SettingFilled />,
-      label: "Autres",
-      children: [
-        { key: "20", label: <Link to="/settings">Configuration</Link> },
-        { key: "21", label: <Link to="/about">Infos</Link> },
-        { key: "22", label: <Link to="/demo">Ant Demos</Link> },
-      ],
     },
   ];
+
+  if (settings?.getDebugMode()) {
+    menuItems.push({
+      key: "misc",
+      icon: <PlusOutlined />,
+      label: "Misc",
+      children: [
+        { key: "devices", label: <Link to="/devices">Appareils</Link> },
+
+        { key: "debug", label: <Link to="/debug">Debug</Link>, icon: <ControlOutlined /> },
+        { key: "demo", label: <Link to="/demo">Ant Demos</Link> },
+        {
+          key: "measures",
+          icon: <PercentageOutlined />,
+          label: <Link to="/sensors">Mesures</Link>,
+        },
+      ],
+    });
+  }
 
   return (
     <AntMenu
       mode="inline"
-      defaultSelectedKeys={["1"]}
-      defaultOpenKeys={["home", "devices"]}
+      defaultSelectedKeys={["home"]} // make this dynamic depending on the current route
+      defaultOpenKeys={["misc"]}
       style={{ height: "100%" }}
       items={menuItems}
     />
