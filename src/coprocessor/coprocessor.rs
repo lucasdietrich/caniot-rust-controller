0f265b94308c6de4cb24db89c3bf769aad19164f
Lucas Dietrich <ld.adecy@gmail.com>
2024-12-15 17:37:09 +0100

    refactor: Introduce keep-alive for connection with BLE copro, improve coprocessor state management

--- src/coprocessor/coprocessor.rs
+++ src/coprocessor/coprocessor.rs
@@ -1,7 +1,8 @@
 use std::time::Duration;
 
 use ble_copro_stream_server::{
-    stream_message::ChannelMessage, xiaomi::XiaomiRecord, StreamServer, Timestamp,
+    stream_channel::StreamChannel, stream_message::ChannelMessage, xiaomi::XiaomiRecord,
+    StreamServer, Timestamp,
 };
 use chrono::Utc;
 use rocket::error;
@@ -33,8 +34,9 @@ pub struct Coprocessor {
 }
 
 enum State {
-    Disconnected,
-    Connected(StreamServer),
+    Uninitialized,
+    Listening(StreamServer),
+    Connected(StreamChannel, StreamServer),
 }
 
 pub struct CoproHandle {
@@ -49,72 +51,78 @@ impl Coprocessor {
             Self {
                 sender,
                 config,
-                state: State::Disconnected,
+                state: State::Uninitialized,
             },
             CoproHandle { receiver },
         )
     }
 
-    async fn notify_stream_channel_status(&mut self, status: CoproStreamChannelStatus) {
-        let _ = self.sender.send(CoproMessage::Status(status)).await;
+    async fn notify_stream_channel_status(
+        sender: &mut mpsc::Sender<CoproMessage>,
+        status: CoproStreamChannelStatus,
+    ) {
+        let _ = sender.send(CoproMessage::Status(status)).await;
+    }
+
+    async fn run_state(
+        state: State,
+        sender: &mut mpsc::Sender<CoproMessage>,
+        config: &CoproConfig,
+    ) -> State {
+        match state {
+            State::Uninitialized => {
+                match StreamServer::init(config.listen_ip.as_str(), config.listen_port).await {
+                    Ok(server) => State::Listening(server),
+                    Err(e) => {
+                        error!("Failed to start server: {}", e);
+                        Self::notify_stream_channel_status(
+                            sender,
+                            CoproStreamChannelStatus::Error("Failed to start server".to_string()),
+                        )
+                        .await;
+                        sleep(COPRO_SERVER_INIT_RETRY_INTERVAL).await;
+                        state
+                    }
+                }
+            }
+            State::Listening(server) => match server.accept().await.ok() {
+                Some(channel) => {
+                    Self::notify_stream_channel_status(sender, CoproStreamChannelStatus::Connected)
+                        .await;
+                    State::Connected(channel, server)
+                }
+                None => {
+                    error!("Failed to accept connection");
+                    Self::notify_stream_channel_status(
+                        sender,
+                        CoproStreamChannelStatus::Error("Failed to accept connection".to_string()),
+                    )
+                    .await;
+                    sleep(COPRO_SERVER_ACCEPT_RETRY_INTERVAL).await;
+                    State::Listening(server)
+                }
+            },
+            State::Connected(mut client, server) => {
+                while let Ok(ChannelMessage::Xiaomi(mut xiaomi_record)) = client.next().await {
+                    // override xiaomi_record timestamp with current time
+                    // TODO this needs to be changed to use Copro timestamp when available
+                    xiaomi_record.timestamp = Timestamp::Utc(Utc::now());
+
+                    let _ = sender.send(CoproMessage::XiaomiRecord(xiaomi_record)).await;
+                }
+
+                info!("Connection closed");
+
+                Self::notify_stream_channel_status(sender, CoproStreamChannelStatus::Disconnected)
+                    .await;
+                State::Listening(server)
+            }
+        }
     }
 
     pub async fn run(mut self) {
         loop {
-            match self.state {
-                State::Disconnected => {
-                    match StreamServer::init(
-                        self.config.listen_ip.as_str(),
-                        self.config.listen_port,
-                    )
-                    .await
-                    {
-                        Ok(server) => {
-                            self.state = State::Connected(server);
-                            self.notify_stream_channel_status(CoproStreamChannelStatus::Connected)
-                                .await;
-                        }
-                        Err(e) => {
-                            error!("Failed to start server: {}", e);
-                            self.notify_stream_channel_status(CoproStreamChannelStatus::Error(
-                                "Failed to start server".to_string(),
-                            ));
-                            sleep(COPRO_SERVER_INIT_RETRY_INTERVAL).await;
-                        }
-                    }
-                }
-                State::Connected(ref server) => {
-                    if let Some(mut client) = server.accept().await.ok() {
-                        info!("Coprocessor stream accepted");
-
-                        while let Ok(ChannelMessage::Xiaomi(mut xiaomi_record)) =
-                            client.next().await
-                        {
-                            // override xiaomi_record timestamp with current time
-                            // TODO this needs to be changed to use Copro timestamp when available
-                            xiaomi_record.timestamp = Timestamp::Utc(Utc::now());
-
-                            let _ = self
-                                .sender
-                                .send(CoproMessage::XiaomiRecord(xiaomi_record))
-                                .await;
-                        }
-
-                        info!("Connection closed");
-
-                        self.state = State::Disconnected;
-                        self.notify_stream_channel_status(CoproStreamChannelStatus::Connected)
-                            .await;
-                    } else {
-                        error!("Failed to accept connection");
-                        self.notify_stream_channel_status(CoproStreamChannelStatus::Error(
-                            "Failed to accept connection".to_string(),
-                        ))
-                        .await;
-                        sleep(COPRO_SERVER_ACCEPT_RETRY_INTERVAL).await;
-                    }
-                }
-            }
+            self.state = Self::run_state(self.state, &mut self.sender, &self.config).await;
         }
     }
 }
